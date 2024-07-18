6c8dd37f4941efb37e359ac38004c90840512062
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-18 20:04:26 +0530

    Update color.h

--- color.h
+++ color.h
@@ -12,12 +12,14 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair; 
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
+
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 }; 
@@ -25,7 +27,8 @@ const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]); 
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
