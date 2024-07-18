0448edb1f7e356f3b48e7e44a29674d687f6521c
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-18 19:37:03 +0530

    Create colour.h

---
+++ colour.h
@@ -0,0 +1,19 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <stdio.h>
+#include <assert.h>
+ 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE}; 
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair); 
+#endif /* COLOR_H */
+ 
